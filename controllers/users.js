44f693eb1e2a4dfc7b3655c4800e9796ce884d97
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-26 11:10:42 -0700

    complete step 2

--- controllers/users.js
+++ controllers/users.js
@@ -32,11 +32,56 @@ router.post('/', async (req, res) => {
             console.log('that email already exists')
             res.render('users/new.ejs', {msg: 'email already taken, idiot'})
         }
-       
-    
     } catch(err) {
         console.log(err)
     }
+})
+
+
+// GET /users/login -- renders a login form
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs', {msg: null})
+})
+
+
+// POST /users/login -- authenticates user credentials against the database
+router.post('/login', async (req, res) => {
+    try {
+        //look up the user in the db based on their email
+        const foundUser = await db.user.findOne({
+            where: {email: req.body.email}
+        })
+        const msg = 'bad login credentials, ur not authenticated'
+        //if the user is not found -- display the login form and give them a message
+        if (!foundUser) {
+            console.log('email not found on login')
+            res.render('users/login.ejs', {msg})
+            return //do not continue with the function
+        }
+        
+        //otherwise, check the provided password against the password in the database
+        //hash the password from the req.boy and compare it to the db password
+        if (foundUser.password === req.body.password) {
+            //if they match -- send the user a cookie! to log them in 
+            res.cookie('userId', foundUser.id)
+            //TODO: redirect to profile
+            res.redirect('/')
+        } else {
+            // if not -- render the login form with a message
+            res.render('users/login.ejs', {msg})
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+// GET /users/logout -- clear the cookie to log the user out
+router.get('/logout', (req, res) => {
+    //clear the cookie from storage
+    res.clearCookie('userId')
+    //redirect to root
+    res.redirect('/')
 })
 
 
